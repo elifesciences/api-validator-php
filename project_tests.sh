0864db339c40bb77da11bb84b1e6394a2cd5e31b
Nathan Lisgo <nathan@aeopus.com>
2021-02-16 11:06:30 +0000

    Replace proofreader with phpcs (#13)

--- project_tests.sh
+++ project_tests.sh
@@ -4,6 +4,6 @@ set -e
 : "${dependencies:?Need to set dependencies environment variable}"
 if [ "$dependencies" = "lowest" ]; then composer update --prefer-lowest --no-interaction; else composer update --no-interaction; fi;
 
-if [ "$dependencies" = "lowest" ]; then proofreader spec/ src/ test/; fi;
+if [ "$dependencies" = "lowest" ]; then vendor/bin/phpcs --standard=phpcs.xml.dist --warning-severity=0 -p spec/ src/ test/; fi;
 vendor/bin/phpunit --log-junit "build/${dependencies}-phpunit.xml"
 (vendor/bin/phpspec run --format=junit | tee "build/${dependencies}-phpspec.xml") && echo "PHPSpec tests passed - see build/${dependencies}-phpspec.xml log"
